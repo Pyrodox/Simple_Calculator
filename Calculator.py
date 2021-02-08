3776a805ffa49a617f6964610ac920bea84031c7
Pyro-Impact <77907654+Pyro-Impact@users.noreply.github.com>
2021-02-07 18:58:53 -0800

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -8,10 +8,13 @@ while True:
     a = usernums()
     b = sign()
     c = usernums()
+
     calculate_by = SetNums(a, c)
     operations = {"+": calculate_by.adding, "-": calculate_by.subtracting, "*": calculate_by.multiplying,
                   "/": calculate_by.dividing, "^": calculate_by.exponent}
+
     print(operations.get(b)())
+
     if confirm_restart() == "yes":
         continue
     else:
